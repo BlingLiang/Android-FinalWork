f4f35b70b1bae5b1233004df52421b0a715e7478
BlingLiang <53328792+BlingLiang@users.noreply.github.com>
2021-10-27 23:14:29 +0800

    【添加首页四个网站图片】

--- AndroidHomework41810018/app/src/main/java/com/swufe/NewsShowing/MainActivity.java
+++ AndroidHomework41810018/app/src/main/java/com/swufe/NewsShowing/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// http://www.mrcj88.cn                            每日财经网      TestListNews1
+// http://money.people.com.cn/GB/218900/index.html 人民网财经新闻  TestListNews2
+// http://news.baidu.com/finance                   百度财经新闻    TestListNews3
+// http://stock.10jqka.com.cn/bkfy_list/           同花顺         TestListNews4
+// https://finance.eastmoney.com                   东方财富       TestListNews5
+// https://www.caijing.com.cn                      中国财经新闻网  TestListNews6
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
