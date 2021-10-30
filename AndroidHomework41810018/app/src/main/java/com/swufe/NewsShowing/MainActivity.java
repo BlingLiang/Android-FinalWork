15c48899baca84e55cf3b1e414d2c6e6aa8d8375
BlingLiang <53328792+BlingLiang@users.noreply.github.com>
2021-10-30 23:49:13 +0800

    【优化首页布局，解决一些小bug】

--- AndroidHomework41810018/app/src/main/java/com/swufe/NewsShowing/MainActivity.java
+++ AndroidHomework41810018/app/src/main/java/com/swufe/NewsShowing/MainActivity.java
@@ -2,7 +2,9 @@ package com.swufe.NewsShowing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 // http://www.mrcj88.cn                            每日财经网      TestListNews1
@@ -15,5 +17,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.t1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestListNews1.class));
+                //通过上面的代码可以实现点击按钮跳转到另一个页面
+                //通过上面的代码可以实现点击按钮跳转网页
+            }
+        });
+        findViewById(R.id.t2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestListNews2.class));
+                //通过上面的代码可以实现点击按钮跳转到另一个页面
+                //通过上面的代码可以实现点击按钮跳转网页
+            }
+        });
+        findViewById(R.id.t3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestListNews3.class));
+                //通过上面的代码可以实现点击按钮跳转到另一个页面
+                //通过上面的代码可以实现点击按钮跳转网页
+            }
+        });
+        findViewById(R.id.t4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestListNews4.class));
+                //通过上面的代码可以实现点击按钮跳转到另一个页面
+                //通过上面的代码可以实现点击按钮跳转网页
+            }
+        });
+        findViewById(R.id.t5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestListNews5.class));
+                //通过上面的代码可以实现点击按钮跳转到另一个页面
+                //通过上面的代码可以实现点击按钮跳转网页
+            }
+        });
+        findViewById(R.id.t6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestListNews6.class));
+                //通过上面的代码可以实现点击按钮跳转到另一个页面
+                //通过上面的代码可以实现点击按钮跳转网页
+            }
+        });
     }
 }
